728d8961eaef6f708121dbfef15eefc7b95c19c0
Alexander Martinz <amartinz@shiftphones.com>
2023-05-05 10:53:06 +0200

    Update wallpapers for sos-5.x
    Change-Id: Ia89abb2d23d6ebf046a20adae36f2d8821754c80
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -22,6 +22,7 @@ LOCAL_MODULE_OWNER := shift
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
