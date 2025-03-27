13a7bde19444b7691f7841b176bb32763d334dc0
Alexander Martinz <amartinz@shiftphones.com>
2025-03-27 13:29:17 +0100

    Wallpapers: resize small variant to 1/2 of original
    Change-Id: I918926caef3a0e3cbe4b8e4f41ccc03d3cb1fc97
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -23,9 +23,9 @@ def generate_smallvariants(resource):
         wallpaper_small = os.path.splitext(wallpaper)[0] + "_small.jpg"
         wallpaper_small_path = os.path.join(wallpapers_path, wallpaper_small)
 
-        # Save the wallpaper with 1/3 size to wallpaper_small_path
+        # Save the wallpaper with 1/2 size to wallpaper_small_path
         with Image.open(os.path.join(wallpapers_path, wallpaper)) as img:
-            size = int(img.width / 3), int(img.height / 3)
+            size = int(img.width / 2), int(img.height / 2)
 
             img_small = img.resize(size, Image.Resampling.LANCZOS)
             img_small.save(wallpaper_small_path, "JPEG")
